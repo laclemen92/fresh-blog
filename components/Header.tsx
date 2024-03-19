86cc9a6fcec1f819e4e68210c85228aebac7e38e
Luke Clement <laclemen92@gmail.com>
2024-03-19 11:36:55 -0500

    chore: removed temporary menu items

--- components/Header.tsx
+++ components/Header.tsx
@@ -78,22 +78,6 @@ export function Header(props: HeaderProps) {
                       aria-labelledby="dropdownDefaultButton"
                     >
                       <li>
-                        <a
-                          href="#"
-                          class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                        >
-                          Settings
-                        </a>
-                      </li>
-                      <li>
-                        <a
-                          href="#"
-                          class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                        >
-                          Earnings
-                        </a>
-                      </li>
-                      <li>
                         <Button
                           href="/signout?success_url=/"
                           // style="secondary"
