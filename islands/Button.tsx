1422527e23a013dbe1f9db4f3af3001f55f1f3fd
Luke Clement <laclemen92@gmail.com>
2024-03-19 08:28:19 -0500

    chore: refactor button component to allow tooltips

--- islands/Button.tsx
+++ islands/Button.tsx
@@ -8,6 +8,9 @@ export interface ButtonProps {
   children: preact.JSX.Element | string;
   onClick?: (e: Event) => Promise<void> | void;
   htmlClass?: string;
+  tooltip?: boolean;
+  tooltipContent?: string;
+  tooltipId?: string;
 }
 
 export function Button(props: ButtonProps) {
@@ -26,6 +29,8 @@ export function Button(props: ButtonProps) {
     },
   };
 
+  const htmlClass = props.htmlClass || "";
+
   if (props.type === "anchor" && props.href && props.style) {
     return (
       <a
@@ -41,20 +46,37 @@ export function Button(props: ButtonProps) {
         {props.children}
       </a>
     );
-  } else if (props.type === "button" && props.style) {
+  } else if (props.type === "button") {
+    const classes: string[] = [
+      ...htmlClass.split(" "),
+    ];
+
+    if (props.style) {
+      classes.push(styles[props.style].background);
+      classes.push(styles[props.style].text);
+      classes.push(styles[props.style].ring);
+      classes.push(styles[props.style].hover);
+    }
+
     return (
-      <button
-        class={`rounded-lg ${
-          styles[props.style].background
-        } px-3 py-2 text-sm font-semibold ${
-          styles[props.style].text
-        } shadow-sm ring-1 ring-inset ${styles[props.style].ring} ${
-          styles[props.style].hover
-        } ${props.htmlClass || ""}`}
-        onClick={props.onClick}
-      >
-        {props.children}
-      </button>
+      <>
+        <button
+          class={`${classes.join(" ") || ""}`}
+          onClick={props.onClick}
+        >
+          {props.children}
+        </button>
+        {props.tooltip && (
+          <div
+            id={props.tooltipId || "tooltip"}
+            role="tooltip"
+            class="absolute z-10 invisible bottom-1 inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700"
+          >
+            {props.tooltipContent}
+            <div class="tooltip-arrow" data-popper-arrow></div>
+          </div>
+        )}
+      </>
     );
   } else if (props.type === "avatar") {
     return (
