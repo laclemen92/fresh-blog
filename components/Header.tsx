a80b42bdf7c499036692c91592a169f28f3b5606
Luke Clement <laclemen92@gmail.com>
2024-03-20 14:02:12 -0500

    feat: Updated styling for dropdown menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import { User } from "@/utils/db.ts";
 import { Avatar } from "@/components/Avatar.tsx";
 import { Button } from "@/islands/Button.tsx";
+import IconPlus from "https://deno.land/x/tabler_icons_tsx@0.0.5/tsx/plus.tsx";
 
 export interface HeaderProps {
   sessionUser?: User;
@@ -30,19 +31,6 @@ export function Header(props: HeaderProps) {
         </div>
         <nav class="flex">
           <ul class="flex justify-center items-center gap-2 sm:gap-4 mx-8 my-2 sm:my-6 flex-wrap md:mx-8">
-            {props.sessionUser && props.sessionUser.role === "admin"
-              ? (
-                <li class="hidden md:flex">
-                  <Button
-                    href="/posts/new"
-                    style="primary"
-                    type="anchor"
-                  >
-                    New Post
-                  </Button>
-                </li>
-              )
-              : null}
             {!props.sessionUser
               ? (
                 <li>
@@ -55,14 +43,13 @@ export function Header(props: HeaderProps) {
                   </Button>
                 </li>
               )
-              : null}
-            {props.sessionUser
-              ? (
-                <li class="flex items-center">
+              : (
+                <li class="relative">
                   <Button
                     onClick={toggleMenu}
                     type="avatar"
                     dataDropdownToggle="header-menu-dropdown"
+                    dataDropdownPlacement="bottom-end"
                   >
                     <Avatar
                       login={props?.sessionUser?.login || null}
@@ -71,29 +58,74 @@ export function Header(props: HeaderProps) {
                   </Button>
                   <div
                     id="header-menu-dropdown"
-                    class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+                    class="z-10 hidden bg-white divide-y divide-gray-100 bg-white border border-gray-300/70 rounded-lg shadow-xl shadow-gray-400/20 w-44 dark:bg-gray-700"
                   >
                     <ul
-                      class="py-2 text-sm text-gray-700 dark:text-gray-200"
+                      class="py-2 text-sm text-gray-700 dark:text-gray-200 divide-y divide-slate-200"
                       aria-labelledby="dropdownDefaultButton"
                     >
+                      <li class="flex flex-col">
+                        <div class="flex items-center justify-center">
+                          <Avatar
+                            login={props?.sessionUser?.login || null}
+                            size={96}
+                            class="mx-4 my-2"
+                          />
+                        </div>
+                        {props?.sessionUser?.name
+                          ? (
+                            <div class="flex items-center justify-center font-bold text-lg">
+                              {props.sessionUser.name}
+                            </div>
+                          )
+                          : null}
+                      </li>
                       {props.sessionUser && props.sessionUser.role === "admin"
                         ? (
-                          <li>
+                          <li class="flex justify-between hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                             <Button
                               href={`/user/${props.sessionUser.login}/posts`}
-                              htmlClass="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                              htmlClass="flex flex-1 block px-4 py-2 text-base"
                               type="anchor"
                             >
-                              My posts
+                              Posts
+                              {/* <div class="flex justify-between"> */}
+                              {/* <div class="">Posts</div> */}
+                              {/* <IconPlus class="w-4 h-4" /> */}
+                              {/* </div> */}
+                            </Button>
+                            <Button
+                              href={`/posts/new`}
+                              htmlClass="block px-4 py-2 text-gray-400 hover:text-gray-900"
+                              type="anchor"
+                            >
+                              <IconPlus class="w-5 h-5" />
                             </Button>
                           </li>
                         )
                         : null}
+                      {
+                        /* <li class="flex justify-between hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                        <Button
+                          href={`/notes`}
+                          htmlClass="flex flex-1 block px-4 py-2 text-base"
+                          type="anchor"
+                        >
+                          Notes
+                        </Button>
+                        <Button
+                          href={`/notes/new`}
+                          htmlClass="block px-4 py-2 text-gray-400 hover:text-gray-900"
+                          type="anchor"
+                        >
+                          <IconPlus class="w-5 h-5" />
+                        </Button>
+                      </li> */
+                      }
                       <li>
                         <Button
                           href="/signout?success_url=/"
-                          htmlClass="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                          htmlClass="block px-4 py-2 text-base hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                           type="anchor"
                         >
                           Sign out
@@ -102,11 +134,9 @@ export function Header(props: HeaderProps) {
                     </ul>
                   </div>
                 </li>
-              )
-              : null}
+              )}
           </ul>
         </nav>
-        {/* mobile menu */}
       </header>
     </>
   );
