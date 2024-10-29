9c0bd28d6bcf1719367e5b1891d2561f49dfe454
Luke Clement <laclemen92@gmail.com>
2024-10-29 09:10:11 -0500

    localstorage issue in deno deploy

--- stores/secretKeyStore.ts
+++ stores/secretKeyStore.ts
@@ -1,30 +1,38 @@
+import { IS_BROWSER } from "$fresh/runtime.ts";
+
 const SECRET_KEY_STORAGE_KEY = "secretKey";
 const timeout = 1000 * 60 * 60 * 24 * 30; // 30 days
 
 const setSecretKey = (key: string) => {
-  localStorage.setItem(SECRET_KEY_STORAGE_KEY, key);
+  if (IS_BROWSER) {
+    localStorage.setItem(SECRET_KEY_STORAGE_KEY, key);
 
-  // Calculate the expiration timestamp
-  const expirationTimestamp = Date.now() + timeout;
+    // Calculate the expiration timestamp
+    const expirationTimestamp = Date.now() + timeout;
 
-  // Store the expiration timestamp in localStorage
-  localStorage.setItem(
-    `${SECRET_KEY_STORAGE_KEY}_expiration`,
-    expirationTimestamp.toString(),
-  );
+    // Store the expiration timestamp in localStorage
+    localStorage.setItem(
+      `${SECRET_KEY_STORAGE_KEY}_expiration`,
+      expirationTimestamp.toString(),
+    );
+  }
 };
 
 const getSecretKey = (): string | null => {
-  const expirationTimestamp = localStorage.getItem(
-    `${SECRET_KEY_STORAGE_KEY}_expiration`,
-  );
+  if (IS_BROWSER) {
+    const expirationTimestamp = localStorage.getItem(
+      `${SECRET_KEY_STORAGE_KEY}_expiration`,
+    );
 
-  if (expirationTimestamp && Date.now() > parseInt(expirationTimestamp)) {
-    localStorage.removeItem(SECRET_KEY_STORAGE_KEY);
-    localStorage.removeItem(`${SECRET_KEY_STORAGE_KEY}_expiration`);
-    return null;
+    if (expirationTimestamp && Date.now() > parseInt(expirationTimestamp)) {
+      localStorage.removeItem(SECRET_KEY_STORAGE_KEY);
+      localStorage.removeItem(`${SECRET_KEY_STORAGE_KEY}_expiration`);
+      return null;
+    }
+    return localStorage.getItem(SECRET_KEY_STORAGE_KEY);
   }
-  return localStorage.getItem(SECRET_KEY_STORAGE_KEY);
+
+  return null;
 };
 
 export { getSecretKey, setSecretKey };
