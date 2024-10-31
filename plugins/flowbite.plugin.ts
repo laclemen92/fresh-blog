51e6bbc53ff023abfddf0ce14f6b62ec656a7e8b
Luke Clement <laclemen92@gmail.com>
2024-10-31 10:34:14 -0500

    remove unused flowbite plugin

--- plugins/flowbite.plugin.ts
+++
@@ -1,6 +0,0 @@
-export default function main() {
-  const source = "https://unpkg.com/flowbite@1.7.0/dist/flowbite.min.js";
-  const script = document.createElement("script");
-  script.src = source;
-  document.head.appendChild(script);
-}
