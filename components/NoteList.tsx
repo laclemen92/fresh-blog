36e66d217ad3875a495a7170b2591704b36e2cef
Luke Clement <laclemen92@gmail.com>
2024-11-01 14:46:22 -0500

    Improve styling to prevent viewing posts or notes from overflowing horizontally, also add a new note button on the NoteList component

--- components/NoteList.tsx
+++ components/NoteList.tsx
@@ -1,5 +1,6 @@
 import { Note } from "@/models/Note.ts";
 import IconUserCircle from "https://deno.land/x/tabler_icons_tsx@0.0.5/tsx/user-circle.tsx";
+import { Button } from "@/islands/Button.tsx";
 
 const formatNoteDate = (noteDate: Date) => {
   noteDate = new Date(noteDate);
@@ -24,38 +25,50 @@ export default function NoteList(
   }
 
   return (
-    <div class="grid grid-cols-1 gap-6 sm:grid-cols-3 lg:grid-cols-4">
-      {notes.map((note) => {
-        note = isDenoNote(note) ? note.value : note;
+    <div>
+      <div class="flex flex-row-reverse gap-2 mb-2">
+        <Button
+          style="primary"
+          type="anchor"
+          htmlClass="float-right rounded-lg px-3 py-2 text-sm font-semibold shadow-sm ring-1 ring-inset"
+          href={`/notes/new`}
+        >
+          New Note
+        </Button>
+      </div>
+      <div class="grid grid-cols-1 gap-6 sm:grid-cols-3 lg:grid-cols-4">
+        {notes.map((note) => {
+          note = isDenoNote(note) ? note.value : note;
 
-        return (note && !note.deleted
-          ? (
-            <div
-              key={note.id}
-              class="border border-gray-300/70 hover:border-gray-900 hover:bg-gray-50 rounded-lg shadow-md shadow-gray-400/20 overflow-hidden"
-            >
-              <a
-                href={`/notes/${note.id}`}
+          return (note && !note.deleted
+            ? (
+              <div
+                key={note.id}
+                class="border border-gray-300/70 hover:border-gray-900 hover:bg-gray-50 rounded-lg shadow-md shadow-gray-400/20 overflow-hidden"
               >
-                <div class="px-4 py-5 sm:px-6">
-                  <h3 class="text-lg font-medium leading-6 text-gray-900">
-                    {note.title}
-                  </h3>
-                  <p class="mt-1 max-w-2xl text-sm text-gray-500">
-                    {note && note?.createdAt
-                      ? formatNoteDate(note.createdAt)
-                      : null}
-                  </p>
-                  <p class="mt-1 max-w-2xl text-sm text-gray-500">
-                    <IconUserCircle class="h-4 w-4 mr-1 inline" />
-                    {note.userLogin}
-                  </p>
-                </div>
-              </a>
-            </div>
-          )
-          : null);
-      })}
+                <a
+                  href={`/notes/${note.id}`}
+                >
+                  <div class="px-4 py-5 sm:px-6">
+                    <h3 class="text-lg font-medium leading-6 text-gray-900">
+                      {note.title}
+                    </h3>
+                    <p class="mt-1 max-w-2xl text-sm text-gray-500">
+                      {note && note?.createdAt
+                        ? formatNoteDate(note.createdAt)
+                        : null}
+                    </p>
+                    <p class="mt-1 max-w-2xl text-sm text-gray-500">
+                      <IconUserCircle class="h-4 w-4 mr-1 inline" />
+                      {note.userLogin}
+                    </p>
+                  </div>
+                </a>
+              </div>
+            )
+            : null);
+        })}
+      </div>
     </div>
   );
 }
